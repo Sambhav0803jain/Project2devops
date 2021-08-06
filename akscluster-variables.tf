dd6d9f3495d7d749dd75fb9a47e79640c3b85dee
Sambhav0803jain <sambhavjain0803@gmail.com>
2021-08-06 21:15:57 +0530

    v2

---
+++ akscluster-variables.tf
@@ -0,0 +1,16 @@
+variable "location" {
+    default = "east us"
+}
+variable "resource_group_name" {
+    default = "akscluster"
+}
+
+variable "cluster_name" {
+    default = "sjtr1234"
+}
+variable "dns_prefix" {
+    default = "sam"
+}
+
+
+
