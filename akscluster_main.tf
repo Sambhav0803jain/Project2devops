0d4a7cc6a322f5f4842dbc03089449612323556b
Sambhav0803jain <sambhavjain0803@gmail.com>
2021-08-17 07:54:57 +0530

    v3

--- akscluster_main.tf
+++ akscluster_main.tf
@@ -9,60 +9,85 @@ tenant_id       = "360286fd-ae5d-4a12-9aee-0790e0d53ca9"
     features {}
 }
 
-resource "random_string" "log_analytics_suffix" {
-    length = 5
-    lower = true
-    special = false
-    number = false
-}
-
-#Create Resource Group
 resource "azurerm_resource_group" "k8s" {
-    name = var.resource_group_name
-    location = var.location
+    name     = "AKC_RG"
+    location = var.AKS_RG_location
 }
 
 
+resource "random_id" "log_analytics_workspace_name_suffix" {
+    byte_length = 8
+}
 
-#Create AKS Cluster
-    resource "azurerm_kubernetes_cluster" "k8s" {
+# azure  log analytics workspace 
+
+
+resource "azurerm_kubernetes_cluster" "k8s" {
     name                = var.cluster_name
     location            = azurerm_resource_group.k8s.location
     resource_group_name = azurerm_resource_group.k8s.name
-    dns_prefix          = var.dns_prefix
+    dns_prefix          = "k8sdns"
 
     linux_profile {
-        admin_username = "vmadmin"
-        
+        admin_username = "ubuntu"
+    
+
         ssh_key {
-            key_data = "${trimspace(tls_private_key.key.public_key_openssh)} vmadmin@azure.com"
+            key_data = "${trimspace(tls_private_key.key.public_key_openssh)} ${var.admin_username}@azure.com"
         }
     }
 
     default_node_pool {
         name            = "agentpool"
-        node_count      = 2
-        max_pods = 50
-        vm_size         = "Standard_D2_v2"
+        node_count      = var.agent_count
+        vm_size         = "Standard_D2s_v3"
     }
 
     service_principal {
-        client_id     = "1d004ebb-2482-440d-82a0-e22b3dfe8025"
-        client_secret = "XsQJ9N-paM38HIYfC_6LZvB_Lp4I_ik1l-"
+        client_id     = var.client_id
+        client_secret = var.client_secret
     }
 
-    addon_profile {
-        oms_agent {
-        enabled                    = true
-        
-        }
-    }
+    # addon is here
 
     network_profile {
         load_balancer_sku = "Standard"
         network_plugin = "kubenet"
     }
 
-    
+    tags = {
+        Environment = "Development"
+    }
+
 }
-    
+resource "tls_private_key" "key" {
+    algorithm = "RSA"
+}
+
+resource "null_resource" "save-key" {
+    triggers = {
+        key = "${tls_private_key.key.private_key_pem}"
+
+    }
+}
+
+#output
+
+output "host" {
+    value = "${azurerm_kubernetes_cluster.k8s.kube_config.0.host}"
+
+}
+
+output "configure" {
+    value = <<configure
+    Run the following commands to configure kubernetes client :
+    $ terraform output kube_config > ~/.kube/aksconfig
+    $ export KUBECONFIG=~/.kube/askconfig
+
+    Test configuration using kubectl
+    $ kubectl get nodes
+
+configure
+
+
+}
